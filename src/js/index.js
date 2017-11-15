67c1e21c5549cf219a88afdf481524eb44025733
Michael Beaujean <mbeaujean88@gmail.com>
2017-11-15 15:10:13 -0500

    adding sass to webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+const css = require('./../sass/style.scss')
+
 import { BurstList } from './components/BurstList';
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin'),
+		ExtractTextPlugin = require('extract-text-webpack-plugin'),
 		HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+			title: 'Trending Audio React',
 			template: __dirname + '/src/index.html',
 			filename: 'index.html',
 			inject: 'body'
+		}),
+		ExtractTextPluginConfig  = new ExtractTextPlugin({
+			filename: 'style.css',
+			disable: false,
+			allChunks: true
 		});
+
 
 module.exports = {
 	entry: __dirname + '/src/js/index.js',
@@ -13,12 +21,19 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node-modules/,
 				loader: 'babel-loader'
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
 			}
 		]
 	},
 	output: {
 		filename: 'app/index.js',
-		path: __dirname + '/dist/js'
+		path: __dirname + '/dist'
 	},
-	plugins: [HTMLWebpackPluginConfig]
+	plugins: [HTMLWebpackPluginConfig, ExtractTextPluginConfig]
 };
